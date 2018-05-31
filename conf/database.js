82b6481601acefa127e298a65e44a6bb9697a6bb
laysen <laysen1213@163.com>
2018-05-31 12:33:32 +0800

    update

--- conf/database.js
+++ conf/database.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : 'root',
+	password : 'ngwse9d2FH12412fsFd',
 	database : 'laysen'
 });
 
